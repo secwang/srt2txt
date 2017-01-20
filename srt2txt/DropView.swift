75e66649805df4cf7998bf621de1c339fcbe4cca
secwang <monadic.io@gmail.com>
2017-01-20 20:12:36 +0800

    basic add

---
+++ srt2txt/DropView.swift
@@ -0,0 +1,118 @@
+//
+//  DropView.swift
+//  srt2txt
+//
+//  Created by secwang on 20/01/2017.
+//  Copyright © 2017 secwang. All rights reserved.
+//
+
+import Cocoa
+
+class DropView: NSView {
+    
+    var filePath: String?
+    var newFile: String?
+    let expectedExt = ["srt"]  //file extensions allowed for Drag&Drop
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        self.wantsLayer = true
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+        
+        register(forDraggedTypes: [NSFilenamesPboardType, NSURLPboardType])
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        // Drawing code here.
+    }
+    
+    override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+        if checkExtension(sender) == true {
+            self.layer?.backgroundColor = NSColor.blue.cgColor
+            return .copy
+        } else {
+            return NSDragOperation()
+        }
+    }
+    
+    func splitFilename(str: String) -> (directory: String, filenameOnly: String, ext: String) {
+        let url = URL(fileURLWithPath: str)
+        return (url.deletingLastPathComponent().path, url.deletingPathExtension().lastPathComponent, url.pathExtension)
+    }
+    
+    fileprivate func checkExtension(_ drag: NSDraggingInfo) -> Bool {
+        guard let board = drag.draggingPasteboard().propertyList(forType: "NSFilenamesPboardType") as? NSArray,
+            let path = board[0] as? String
+            else { return false }
+        
+        let suffix = URL(fileURLWithPath: path).pathExtension
+        for ext in self.expectedExt {
+            if ext.lowercased() == suffix {
+                return true
+            }
+        }
+        return false
+    }
+    
+    override func draggingExited(_ sender: NSDraggingInfo?) {
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+    }
+    
+    override func draggingEnded(_ sender: NSDraggingInfo?) {
+        self.layer?.backgroundColor = NSColor.gray.cgColor
+    }
+    
+    override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+        guard let pasteboard = sender.draggingPasteboard().propertyList(forType: "NSFilenamesPboardType") as? NSArray,
+            let path = pasteboard[0] as? String
+            else { return false }
+        
+        //GET YOUR FILE PATH !!
+        self.filePath = path
+        Swift.print("FilePath: \(filePath)")
+        var text2 = ""
+        let fileUrl = URL(fileURLWithPath: filePath!)
+
+        
+        if (self.filePath!.isEmpty){
+           Swift.print("finish job")
+        } else {
+            
+            do {
+                text2 = try String(contentsOf: fileUrl, encoding: String.Encoding.utf8)
+            }
+            catch {
+                Swift.print("Error info: \(error)")
+            }
+        }
+        
+        if(text2 != ""){
+            let savePanel = NSSavePanel()
+            savePanel.allowedFileTypes = ["txt"]
+            
+            var newFileName = fileUrl.deletingPathExtension().lastPathComponent
+
+            savePanel.nameFieldStringValue = newFileName
+            let current = NSApplication.shared().mainWindow
+
+
+            savePanel.beginSheetModal(for: current!, completionHandler: { (result) in
+                if result == NSFileHandlingPanelOKButton {
+                    let writePath = savePanel.url
+                    
+                    do {
+                        try text2.write(to: writePath! , atomically: false, encoding: String.Encoding.utf8)
+                    }
+                    catch {
+                        Swift.print("error is \(error)")
+                    }
+                }
+            })
+        }
+        
+        return true
+    }
+    
+}
