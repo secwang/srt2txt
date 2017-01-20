d051ac174ee1b3df452d6d2b40a5b36609374e6d
secwang <monadic.io@gmail.com>
2017-01-20 17:25:58 +0800

    Initial Commit

---
+++ srt2txt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  srt2txt
+//
+//  Created by secwang on 20/01/2017.
+//  Copyright © 2017 secwang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ srt2txt/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  srt2txt
+//
+//  Created by secwang on 20/01/2017.
+//  Copyright © 2017 secwang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
